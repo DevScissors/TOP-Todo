7e4582558a7f878f2fe465acc0b844d6a10ed4fe
DevScissors <sean.scissors@gmail.com>
2026-06-18 15:51:21 -0700

    Making good progress on styling the nav menu, still deciding on overall color scheme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles/style.css";
+
+const navItems = document.querySelectorAll(".menu");
+
+navItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", (e) => {
+    navItems.forEach((nav) => nav.classList.remove("active"));
+    e.currentTarget.classList.toggle("active");
+  });
+});
