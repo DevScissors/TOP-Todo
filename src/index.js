25cad980f05fadf1cf8aebf7a963f3dabf11e537
DevScissors <sean.scissors@gmail.com>
2026-06-21 15:09:12 -0700

    Update the styling again, but also build out the dialog/modal and make a working add task button to bring up the modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import "./styles/style.css";
 
+let taskList = [];
+const modal = document.querySelector("#addNewTask");
+const addTaskBtn = document.querySelector(".new-task-btn");
+
+class ToDo {
+  constructor(name, category, priority, dueDate, status, createdDate, id) {
+    this.name = name;
+    this.category = category;
+    this.priority = {
+      low: "low",
+      normal: "normal",
+      high: "high",
+    };
+    this.dueDate = new Date();
+    this.status = status;
+    this.createdDate = new Date();
+    this.id = crypto.randomUUID();
+  }
+}
+
+const toDoItem = new ToDo(
+  "Wash car",
+  "Personal",
+  "Normal",
+  "12/01/2026",
+  false,
+  "06/21/2026",
+);
+
 const navItems = document.querySelectorAll(".menu");
 
 navItems.forEach((menuItem) => {
@@ -7,4 +36,8 @@ navItems.forEach((menuItem) => {
     navItems.forEach((nav) => nav.classList.remove("active"));
     e.currentTarget.classList.toggle("active");
   });
+});
+
+addTaskBtn.addEventListener("click", () => {
+  modal.showModal();
 });
