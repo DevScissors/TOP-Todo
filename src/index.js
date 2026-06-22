1035262c40179451f880cd0721a602e6ae99544a
DevScissors <sean.scissors@gmail.com>
2026-06-21 21:25:06 -0700

    Added a working modal, just need to connect the content to localStorage as well as link it to the task item display

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./styles/style.css";
 let taskList = [];
 const modal = document.querySelector("#addNewTask");
 const addTaskBtn = document.querySelector(".new-task-btn");
+const taskNameInput = document.querySelector(".task-name-input");
+const taskCategoryInput = document.querySelector(".task-category-input");
+const taskPrioritySelection = document.querySelector(".task-priority-select");
+const taskDueDateInput = document.querySelector(".task-due-by-date-input");
 
 class ToDo {
   constructor(name, category, priority, dueDate, status, createdDate, id) {
@@ -15,19 +19,29 @@ class ToDo {
     };
     this.dueDate = new Date();
     this.status = status;
-    this.createdDate = new Date();
+    this.createdDate = createdDate;
     this.id = crypto.randomUUID();
   }
 }
 
-const toDoItem = new ToDo(
-  "Wash car",
-  "Personal",
-  "Normal",
-  "12/01/2026",
-  false,
-  "06/21/2026",
-);
+function addTaskToList() {
+  const taskNameValue = taskNameInput.value;
+  const taskCategoryValue = taskCategoryInput.value;
+  const taskPriorityValue = taskPrioritySelection.value;
+  const taskDueDateValue = taskDueDateInput.value;
+  const taskDefaultStatusValue = "Not Completed";
+  const taskCreatedDate = new Date().getFullYear();
+
+  const toDoTask = new ToDo(
+    taskNameValue,
+    taskCategoryValue,
+    taskPriorityValue,
+    taskDueDateValue,
+    taskDefaultStatusValue,
+    taskCreatedDate,
+  );
+  taskList.push(toDoTask);
+}
 
 const navItems = document.querySelectorAll(".menu");
 
